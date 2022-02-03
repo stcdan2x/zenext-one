3b11cea2162d5c1ce764c11272f7798757e6ab23
Steven Dandan <stcdan2x@gmail.com>
2022-02-03 21:14:55 +0800

    creates initial pages and components including custom app.tsx and custom document.tsx

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Index = () => {
+	return (
+		<Layout>
+			<div>Layout in Index Component</div>
+		</Layout>
+	);
+};
+
+export default Index;
