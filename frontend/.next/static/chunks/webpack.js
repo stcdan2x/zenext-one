cb569bde98c7504122035ca796c5ca9c919aaf61
Steven Dandan <stcdan2x@gmail.com>
2022-02-12 23:47:22 +0800

    modified layout component

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca6b6fce158e7a7d2b2e"; }
+/******/ 		__webpack_require__.h = function() { return "b246f5c0c804b85936e9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
